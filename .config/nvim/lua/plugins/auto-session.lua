601470f50c6970c71b611fdf1a60bc22f43aa146
Josh Fullmer <joshfullmer9@gmail.com>
2024-07-13 13:39:37 -0500

    Updating which-key after breaking changes

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -7,10 +7,5 @@ return {
       auto_restore_enabled = false,
       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
     })
-
-    -- local keymap = vim.keymap
-
-    -- keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "[R]estore session" }) -- restore last workspace session for current directory
-    -- keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "[S]ave session" }) -- save workspace session for current working directory
   end,
 }
